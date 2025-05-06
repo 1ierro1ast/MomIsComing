71de63878a05778f07d18d87bbb0ce01e28e2744
ddenis1995 <davylyn@gmail.com>
2025-05-07 02:47:00 +0400

    Arranged living room
    Feels like there is too much going on in that room though

---
+++ Assets/MomIsComing/Scripts/PlayerController/ObjectTaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace MomIsComing.Scripts.PlayerController
+{
+    public class ObjectTaker : MonoBehaviour
+    {
+        [SerializeField] private float _takeDistance;
+        [SerializeField] private Transform _eyes;
+        [SerializeField] private LayerMask _interactableLayer;
+        
+        
+        private void Update()
+        {
+            if (Physics.Raycast(_eyes.position, _eyes.forward, out RaycastHit hit, _takeDistance, _interactableLayer))
+            {
+                
+            }
+        }
+    }
+}
